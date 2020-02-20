0330e646b816de76cc78531e90f116ebb5ecfd24
AndreiManache <manacheandrei@gmail.com>
2020-02-20 10:57:04 +0200

    upd1

---
+++ script.js
@@ -0,0 +1,27 @@
+let result;
+
+async function getList(){
+    let response = await fetch("https://baza-de-date-project.firebaseio.com/.json");
+    result = await response.json();
+
+    draw();
+}
+
+
+function draw(){
+    let str = "";
+
+
+for(let i in result){
+    str += `
+    <div class="col-xs-12 col-sm-6 col-lg-4">
+        <div class="box">
+            <img src="${result[i].image}" alt="imagine">
+            <p>${result[i].name}</p>
+            <button>Detalii</button>
+        </div>
+    </div>`;
+}
+    document.querySelector(".wrapper").innerHTML = str;
+
+}
