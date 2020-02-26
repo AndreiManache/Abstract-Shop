25b4566fd0ad96e76259dd080729928a261fe3c4
AndreiManache <manacheandrei@gmail.com>
2020-02-26 13:23:48 +0200

    upddd

--- detalii.js
+++ detalii.js
@@ -16,3 +16,4 @@ function draw(){
     document.querySelector("#detalii").innerHTML = item.about;
 
 }
+
--- script.js
+++ script.js
@@ -15,8 +15,9 @@ async function getList(){
 function draw(){
     let str = "";
 
-
 for(let i in result){
+
+    
     str += `<div class="col-xs-12 col-sm-6 col-lg-4">
 
                 <div class="box">
@@ -36,7 +37,53 @@ for(let i in result){
 
                 </div>
             </div>`;
+          
 }
     document.querySelector(".wrapper").innerHTML = str;
+
+}
+
+function drawSearch(){
+    let str = "";
+    let input = document.querySelector("#nav li input").value;
+
+for(let i in result){
+
+    if(result[i].name.toLowerCase().indexOf(input) > -1 || result[i].about.toLowerCase().indexOf(input) > -1){
+
+        document.querySelector(".backGround").style.display = "none";
+        document.querySelector("#message").innerHTML = `Search results for "${input}"... `;
+
+    
+    str += `<div class="col-xs-12 col-sm-6 col-lg-4">
+
+                <div class="box">
+
+                    <a href="detalii.html?idItem=${[i]}" id="item" target="_blank">
+
+                            <img src="${result[i].image}" alt="imagine">
+                            
+
+                        <div class="access">
+
+                            <span id="name">${result[i].name}</span>
+                            <span>${result[i].price} $</span>
+                    
+                        </div>
+                    </a>
+
+                </div>
+            </div>`;
+          }else{
+            document.querySelector(".backGround").style.display = "none";
+            document.querySelector("#message").innerHTML = `Oopss! there are no results for "${input}"... `;
+          }  
+}
+    document.querySelector(".wrapper").innerHTML = str;
+
+    if(input === ""){
+        document.querySelector("#message").style.display = "none";
+        document.querySelector(".backGround").style.display = "flex";
+    }
 
 }
