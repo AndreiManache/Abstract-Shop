b2c478d21be52b5c74bbab9c047df640e4f9f352
AndreiManache <manacheandrei@gmail.com>
2020-03-28 17:01:35 +0200

    Proiect 98%

--- admin.js
+++ admin.js
@@ -1,25 +1,49 @@
 let result;
 let btn = document.querySelector("#add_btn");
 
+async function getDb(){
+
+    let response = await fetch ("https://baza-de-date-project.firebaseio.com/.json");
+    result = await response.json();
+
+    drawAdmin();
+}
+
 btn.addEventListener('click', function(){
     document.querySelector(".bg-modal-admin").style.display = "block";
 
-    let btn2 = document.querySelector('#btn2');
-    let btn3 = document.querySelector('#btn3');
+    let save = document.querySelector('#btn2');
+    let cancel = document.querySelector('#btn3');
 
-    btn3.addEventListener('click', function(){
+    save.addEventListener('click', function(){
+        saveProduct();
+        document.querySelector(".bg-modal-admin").style.display = "none";
+        getDb();
+    })
+
+    cancel.addEventListener('click', function(){
         document.querySelector(".bg-modal-admin").style.display = "none";
     })
 
 });
 
 
-async function getDb(){
+function saveProduct(){
+    
+    fetch("https://baza-de-date-project.firebaseio.com/.json", {
+    method : "POST",
+    
+    body :JSON.stringify({
+        image: document.getElementById("image_value").value,
+        name : document.getElementById("name_value").value,
+        about: document.getElementById("description_value").value,
+        price: document.getElementById("price_value").value,
+        stock: document.getElementById("stock_value").value,
+    })
+})
+.then(res =>{return res.json()} )
+.then(data => console.log(data))
 
-     let response = await fetch ("https://baza-de-date-project.firebaseio.com/.json");
-     result = await response.json();
-
-     drawAdmin();
 }
 
 
@@ -45,7 +69,7 @@ function drawAdmin(){
                     <p>${result[i].stock}</p>
                 </td>
                 <td class="row_data">
-                    <button onclick="erase(event)">Remove</button>
+                    <button data-id="${i}" onclick="erase(event)">Remove</button>
                 </td>
             </tr></a>`
 
@@ -63,8 +87,24 @@ function drawSearch(){
     return;
 }
 
-function erase(event){
+async function erase(event){
+    event.target.parentNode.parentNode.remove();
+    let id = event.target.dataset.id;
 
-    event.target.parentNode.parentNode.remove(); 
+    let response = await fetch ("https://baza-de-date-project.firebaseio.com/.json");
+     result = await response.json();
 
+    for(let i in result){
+        let product = result[i];
+
+        if(product === null){
+            continue
+        }
+        if(i === id){
+          let deleteResponse = await fetch(`https://baza-de-date-project.firebaseio.com/${id}.json`, 
+          {method : "DELETE"}
+          )
+        }
+
+    }
 }
